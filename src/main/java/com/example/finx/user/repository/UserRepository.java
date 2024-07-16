1f43041ccd6577141b7c8c3f82f699b1d0624c84
imhanseong <hans_hanseong@dsm.hs.kr>
2024-07-16 16:03:24 +0900

    유저 서비스 생성

--- src/main/java/com/example/finx/user/repository/UserRepository.java
+++ src/main/java/com/example/finx/user/repository/UserRepository.java
@@ -3,7 +3,10 @@ package com.example.finx.user.repository;
 
 import com.example.finx.user.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
-
+    Optional<User> findByusername(String username);
 }
