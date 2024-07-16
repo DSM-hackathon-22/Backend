c7dfc88607e233aaffbeedf55a20442dc37da571
kimjoeul <joeul13@naver.com>
2024-07-16 14:13:17 +0900

    프로젝트 시작

---
+++ finx/src/main/java/com/example/finx/FinxApplication.java
@@ -0,0 +1,13 @@
+package com.example.finx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinxApplication.class, args);
+    }
+
+}
